825b9c80e96cca315cf289afd04b38d08acc0af7
pbowman0 <paigeelisabethbowman@gmail.com>
2021-10-04 10:16:20 -0500

    Initial Commit

---
+++ iDine0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine0
+//
+//  Created by Student on 10/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine0/iDine0App.swift
@@ -0,0 +1,17 @@
+//
+//  iDine0App.swift
+//  iDine0
+//
+//  Created by Student on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct iDine0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
