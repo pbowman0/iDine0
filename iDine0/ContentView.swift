3659abaf1e0ee86ba1b5ab38613b5307b75f5bcc
pbowman0 <paigeelisabethbowman@gmail.com>
2021-10-07 10:44:38 -0500

    Observable objects, environment objects, and @Published

--- iDine0/ContentView.swift
+++ iDine0/ContentView.swift
@@ -7,17 +7,20 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     
     var body: some View {
+        
         NavigationView {
             List {
                 ForEach(menu) { section in
                     Section(header: Text(section.name)) {
                         ForEach(section.items) { item in
                             NavigationLink(destination: ItemDetail(item: item)){
-                            ItemRow(item: item)
+                                ItemRow(item: item)
                             }
                         }
                     }
--- iDine0/iDine0App.swift
+++ iDine0/iDine0App.swift
@@ -7,11 +7,16 @@
 
 import SwiftUI
 
+
 @main
 struct iDine0App: App {
+    
+    @StateObject var order = Order()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
